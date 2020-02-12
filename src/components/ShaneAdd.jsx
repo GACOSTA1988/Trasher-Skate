09f9ca2cb46a528dc89bc7cf4ab6bc2a2e82fab6
Student <example@example.com>
2020-02-12 13:38:35 -0800

    build basic site

---
+++ src/components/ShaneAdd.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// Import the styles
+
+
+
+
+function ShaneAdd(props) {
+  var shaneAddStyles = {
+    marginBottom: '90%',
+    // padding: '.25%',
+    border:'15px solid black',
+    width: '80%',
+   };
+  return (
+    <div >
+    <header>
+      <img style={shaneAddStyles} src="https://tpc.googlesyndication.com/pagead/imgad?id=CICAgKDn4ILSGhABGAEyCJx0Bsemo0An" ></img>
+      </header>
+    </div>
+  );
+}
+
+
+
+
+export default ShaneAdd;
